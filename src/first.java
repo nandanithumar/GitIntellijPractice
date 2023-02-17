8afb7a6cdd1c0cd08aa641ce94571f1bcdf11906
Nandani@1234 <nthumar@argusoft.com>
2023-02-17 14:21:29 +0530

    adding one file in first branch

---
+++ src/first.java
@@ -0,0 +1,15 @@
+ class first {
+    private String name;
+
+     public first(String name) {
+         this.name = name;
+     }
+
+     public String getName() {
+         return name;
+     }
+
+     public void setName(String name) {
+         this.name = name;
+     }
+ }
